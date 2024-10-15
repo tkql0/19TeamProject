60f6b5d4786a3a7e69530b67319aad9192e004e8
tkql0 <96943505+tkql0@users.noreply.github.com>
2024-10-16 02:57:15 +0900

    [Complex]Ball_KIM
    + Phtsics Material 2D 추가
    + Reset() - 위치 초기화
    초기 위치에 따라 값은 달라질 수 있음
    + LaunchBall() - 처음 랜덤 팅기기

--- Assets/Prefab_Personal/Kim/Kim_Scripts/Ball.cs
+++ Assets/Prefab_Personal/Kim/Kim_Scripts/Ball.cs
@@ -5,55 +5,68 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    Rigidbody2D rigid2D;
-
-    Vector2 myPos;
-
-    [SerializeField]LayerMask target;
+    private Rigidbody2D rigid2D;
+    private Vector2 myPos;
 
     [SerializeField]
-    private float speed;
+    private LayerMask target;
+
+    public float speed;
+
+    private bool isStart;
 
     private void Awake()
     {
         rigid2D = GetComponent<Rigidbody2D>();
     }
 
-    private void Start()
+    private void OnEnable()
     {
-        LaunchBall();
+        isStart = false;
+        Reset();
     }
 
     private void Update()
     {
-        myPos = transform.position;
-
-        // TODO :: 사라질 높이 변경
-        if(myPos.y < -5f)
+        if(Input.GetKeyDown("Space") && !isStart)
         {
-            //GameManager.instance.MissBall(this.gameObject);
+            isStart = true;
+            LaunchBall();
         }
     }
 
     private void LaunchBall()
     {
         Vector2 jumpVelocity = Vector2.up * speed;
-        jumpVelocity.x = Random.Range(-2f, 2f);
+        jumpVelocity.x = Random.Range(-4f, 4f);
 
         rigid2D.AddForce(jumpVelocity, ForceMode2D.Impulse);
     }
 
+    public void Reset()
+    {
+        // TODO :: SpawnPosition Reset
+        rigid2D.velocity = Vector2.zero;
+        transform.position = Vector2.zero;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
         //if (collision.gameObject.TryGetComponent<???>(out var out???))
         //{
         //GameManager.instance.currentScore += out???.score;
-        //GameManager.instance.currentScore += 5;
+
+        // TODO :: target = collision.gameObject.layer
         if (collision.gameObject.layer == target)
         {
+            // TODO :: 5 = collision.gameObject.class.Score
+            //GameManager.Instance.currentScore += 5;
             Destroy(collision.gameObject);
         }
-        //}
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameManager.Instance.MissBall(gameObject);
     }
 }
