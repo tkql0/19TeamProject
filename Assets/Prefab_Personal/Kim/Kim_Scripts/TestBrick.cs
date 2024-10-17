e1a5701eda3736f618f4bd8d182fe159bec7b1ab
tkql0 <96943505+tkql0@users.noreply.github.com>
2024-10-17 19:47:20 +0900

    [Feature]_TestBrick [Feature]_Prefab
    충돌 위험을 줄이기 위해 개인 폴더에 따로 저장
    
    애니메이션을 추가하기 위해 충돌체인 콜라이더 먼저 비활성화 시켰습니다.
    
    공엔 데미지 블록엔 체력을 추가했고 체력이 0이 되었다면 비활성화 시키도록 했습니다.
    블록이 죽으면 게임매니저의 점수가 올라가도록 연결했습니다.
    
    블록을 움직이려 했지만 움직이지 않아 현재 이유를 찾고 있습니다. 빠른 시일 내에 해결하겠습니다.
    
    블록이 바닥에 닿게 되면 게임 종료 메서드가 실행 되도록 했습니다.

---
+++ Assets/Prefab_Personal/Kim/Kim_Scripts/TestBrick.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBrick : MonoBehaviour
+{
+    public int heart;
+    public int score = 5;
+
+    //private Animator anim;
+    private BoxCollider2D boxCollider;
+
+    public float speed = 0.1f;
+
+    public bool isSpecialLevel;
+
+    private void Awake()
+    {
+        //anim = GetCompoment<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnEnable()
+    {
+        boxCollider.enabled = true;
+        //anim.isDie.SetBool(false);
+        heart = 1;
+        // 게임 레벨이 있는 스크립트에서 받아오기
+        isSpecialLevel = true;
+    }
+
+    private void Update()
+    {
+        Movement();
+
+        if (heart <= 0)
+        {
+            // 콜라이더가 사라지고 공이 통과는 것을 확인
+            boxCollider.enabled = false;
+            // 애니메이션이 있는 오브젝트를 생성하면 되나?
+            //anim.isDie.SetBool(true);
+            Invoke("Dead", 2f);
+            return;
+        }
+    }
+
+    private void Movement()
+    { // 조금씩 벽돌이 내려오게 하기
+        // 내려오는 벽돌이랑 공이랑 부딯쳤을때 공의 속도가
+        // 줄어드는지 보고싶은데 벽돌이 안내려오네
+        Vector2 vectorZero = Vector2.zero;
+
+        transform.position = Vector2.SmoothDamp(transform.position
+            , transform.position + new Vector3(0, -1), ref vectorZero, 5f);
+    }
+
+    public void Dead()
+    {
+        GameManager.Instance.currentScore += score;
+        gameObject.SetActive(false);
+    }
+
+    public void OnCollisionEnter2D(Collision2D inCollision)
+    {
+        if (inCollision.gameObject.TryGetComponent<Ball>(out var outBall))
+        {
+            if (isSpecialLevel)
+            { // 특수 레벨일 경우 데미지 감소 없음
+                return;
+            }
+
+            if (heart > 0)
+            {
+                heart -= outBall.damage;
+            }
+        }
+    }
+
+    // 그러네 블록이 바닥에 닿았을 상황도 있었지
+    private void OnTriggerEnter2D(Collider2D incollision)
+    { // 벽돌이 바닥에 닿았는가?
+        GameManager.Instance.GameOver();
+    }
+}
+
