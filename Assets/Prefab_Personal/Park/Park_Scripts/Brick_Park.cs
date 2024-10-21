9cbfb5aa3bdae6f92c79221c468cea2ae879f22b
Eunseol Park <kongpark2019@gmail.com>
2024-10-21 11:13:43 +0900

    [Feature]_BrickCrack_Park
    벽돌 HIT가 추가되어
    보/빨 3 | 주/노 2 | 초/파 1 이렇게 되어있으며 맞을 때마다 이미지 변경됩니다.

--- Assets/Prefab_Personal/Park/Park_Scripts/Brick_Park.cs
+++ Assets/Prefab_Personal/Park/Park_Scripts/Brick_Park.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class Brick_Park : MonoBehaviour
 {
+    public int hitPoints;
+    
     public GameObject ball;
-
     public GameObject bar;
+    public Sprite[] crackSprites; // Assign the crack sprites in the Inspector
+    private SpriteRenderer spriteRenderer;
+    
     // Start is called before the first frame update
     void Start()
     {
         ball = GetComponent<GameObject>();
         bar = GetComponent<GameObject>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        // Assign hitPoints based on brick color
+        switch (gameObject.tag) // Assume you have tagged bricks by their color
+        {
+            case "PurpleBrick":
+            case "RedBrick":
+                hitPoints = 3; // 3 hits for purple and red bricks
+                break;
+            case "OrangeBrick":
+            case "YellowBrick":
+                hitPoints = 2; // 2 hits for orange and yellow bricks
+                break;
+            case "GreenBrick":
+            case "BlueBrick":
+                hitPoints = 1; // 1 hit for green and blue bricks
+                break;
+        }
     }
 
     public void OnCollisionEnter2D(Collision2D inCollision)
@@ -23,6 +45,27 @@ public class Brick_Park : MonoBehaviour
         else if (inCollision.gameObject == bar)
         {
             //게임 ??
+        }
+    }
+    
+    // Call this method when the ball hits the brick
+    public void TakeDamage()
+    {
+        hitPoints--;
+
+        if (hitPoints > 0)
+        {
+            // Only update sprite for bricks with more than 1 HP (purple, red, orange, yellow)
+            if (crackSprites.Length > 0 && (gameObject.tag == "PurpleBrick" || gameObject.tag == "RedBrick" ||
+                                            gameObject.tag == "OrangeBrick" || gameObject.tag == "YellowBrick"))
+            {
+                spriteRenderer.sprite = crackSprites[hitPoints - 1]; // Show appropriate cracked sprite
+            }
+        }
+        else
+        {
+            // Destroy the brick when HP reaches 0
+            Destroy(gameObject);
         }
     }
 }
