6455e341cb916a3b894d8cb18cd01e1aa573eb41
Eunseol Park <kongpark2019@gmail.com>
2024-10-21 21:52:09 +0900

    [Add]brickscore_Park

--- Assets/Prefab_Personal/Park/Park_Scripts/Brick_Park.cs
+++ Assets/Prefab_Personal/Park/Park_Scripts/Brick_Park.cs
@@ -10,6 +10,7 @@ public class Brick_Park : MonoBehaviour
     public GameObject bar;
     public Sprite[] crackSprites; // Assign the crack sprites in the Inspector
     private SpriteRenderer spriteRenderer;
+    private ItemManager itemManager;
     
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,7 @@ public class Brick_Park : MonoBehaviour
         ball = GetComponent<GameObject>();
         bar = GetComponent<GameObject>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        itemManager = GetComponent<ItemManager>();
         
         // Assign hitPoints based on brick color
         switch (gameObject.tag)
@@ -57,15 +59,16 @@ public class Brick_Park : MonoBehaviour
                 spriteRenderer.sprite = crackSprites[hitPoints - 1]; // Show appropriate cracked sprite
             }
         }
-        else // hitpoints <= 0
+        else //hitpoints <= 0
         {
-            Destroy(gameObject);//setactive(false)
+            itemManager.SpawnRandomItem(transform.position);
+            this.gameObject.SetActive(false);
             
             //Add score
             GameManager.Instance.currentScore += 10;
 
-            //if bricks is null call gave over methods from game manager
-            if (FindObjectsOfType<Brick_Park>().Length == 1) // Assuming this is the last brick
+            //if bricks is null call game over methods from game manager
+            if (FindObjectsOfType<Brick_Park>().Length == 0) // Assuming this is the last brick
             {
                 GameManager.Instance.GameOver(true);
             }
