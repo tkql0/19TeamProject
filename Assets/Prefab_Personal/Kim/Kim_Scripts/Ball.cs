1ae94e657e2aef54df5e0b4ef15d5c786f7558c0
tkql0 <96943505+tkql0@users.noreply.github.com>
2024-10-22 09:05:09 +0900

    [Asset]Anim
    - 공 두배 이벤트에서 사용될 이미지 추가

--- Assets/Prefab_Personal/Kim/Kim_Scripts/Ball.cs
+++ Assets/Prefab_Personal/Kim/Kim_Scripts/Ball.cs
@@ -6,56 +6,42 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public Rigidbody2D rigid2D;
+    private Animator anim;
 
     public float minSpeed;
     public float maxSpeed;
 
     public int damage = 0;
 
-    //public int 
+    public int DoubleBallCount;
 
 
     private void Awake()
     {
+        anim = GetComponent<Animator>();
         rigid2D = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable()
     {
         LaunchBall();
+        anim.SetBool("isBoom", false);
+    }
+
+    private void Update()
+    {
+        //    anim.SetBool("isBoom", true);
     }
 
     private void FixedUpdate()
     {
-        // if (!GameManager.Instance.isGameStart)
-        //{
-          //  return;
-        //}
-
-        Vector2 moveVec = transform.position;
-        float speed = moveVec.magnitude;
-
-        if (speed < minSpeed)
+        if (rigid2D.velocity.magnitude < minSpeed)
         {
-            //if (Mathf.Abs(rigid2D.velocity.x) < minSpeed)
-            //{
-            //    rigid2D.velocity = new Vector2(Mathf.Sign(rigid2D.velocity.x)
-            //        * minSpeed, rigid2D.velocity.y);
-            //}
-            //else if (Mathf.Abs(rigid2D.velocity.y) < minSpeed)
-            //{
-            //    rigid2D.velocity = new Vector2(rigid2D.velocity.x,
-            //        Mathf.Sign(rigid2D.velocity.y) * minSpeed);
-            //}
-
-            if (rigid2D.velocity.magnitude < minSpeed)
-            {
-                rigid2D.velocity = rigid2D.velocity.normalized * minSpeed;
-            }
-            else if(rigid2D.velocity.magnitude > maxSpeed)
-            {
-                rigid2D.velocity = rigid2D.velocity.normalized * maxSpeed;
-            }
+            rigid2D.velocity = rigid2D.velocity.normalized * minSpeed;
+        }
+        else if (rigid2D.velocity.magnitude > maxSpeed)
+        {
+            rigid2D.velocity = rigid2D.velocity.normalized * maxSpeed;
         }
     }
 
